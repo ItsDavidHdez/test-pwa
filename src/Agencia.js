d0bbe3cb98339ab6614fc7ee2817bdb52e8a94da
ItsDavidHdez <david-0610@live.com.mx>
2021-10-21 20:24:29 -0500

    Act 2 done

--- src/Agencia.js
+++ src/Agencia.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { ItemContext } from "./context/ItemContext";
 
 const Agencia = () => {
+  const { item } = React.useContext(ItemContext);
+
+  const [modeloState, setModeloState] = useState(item.modelo);
+  const [marcaState, setMarcaState] = useState(item.marca);
+  const [agenciaState, setAgenciaState] = useState(item.agencia);
+
+  let loading = useRef("");
+
+  React.useEffect(() => {
+    if (!modeloState || !marcaState || !agenciaState) {
+      loading.current = "Sin datos... :(";
+    }
+  }, [modeloState, marcaState, agenciaState]);
+
   return (
     <>
       <h1>Agencia</h1>
+      {!loading.current ? (
+        <p>
+          {modeloState}, {marcaState}, {agenciaState}
+        </p>
+      ) : (
+        <p>{loading.current}</p>
+      )}
     </>
   );
 };
